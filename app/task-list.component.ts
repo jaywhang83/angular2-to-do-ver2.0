9cce97b87d722d7935bf16df47e3320020a84e0f
Michael Gallegos <mklgallegos@gmail.com>
2016-03-22 14:27:56 -0700

    finish More Angular forms: using local variables

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,12 +2,16 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
+
+
+
 
 @Component({
 	selector: 'task-list',
 	inputs: ['taskList'],
 	outputs: ['onTaskSelect'],
-	directives: [TaskComponent, EditTaskDetailsComponent], // TaskComponent is child, EditTaskDetailsComponent is child
+	directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent], // these are all children
 	//templateUrl: 'app/task-list.component.html' // If we use templateUrl, we just have to remember that the path is relative to the top level of your project directory because this is where we start our server. So, it's important to include the app/ at the beginning of the templateUrl path, otherwise it won't be located.
 	template:
 	`
@@ -18,6 +22,7 @@ import { EditTaskDetailsComponent } from './edit-task-details.component';
 	</task-display>
 	<edit-task-details *ngIf="selectedTask" [task]="selectedTask">
 	</edit-task-details>
+	<new-task (onSubmitNewTask)="createTask($event)"></new-task>
 	`
 })
 
@@ -28,9 +33,16 @@ export class TaskListComponent {
 	constructor() {
 		this.onTaskSelect = new EventEmitter();
 	}
+
 	taskClicked(clickedTask: Task): void {
 		console.log('CHILD', clickedTask);
 		this.selectedTask = clickedTask;
 		this.onTaskSelect.emit(clickedTask);
+	}
+
+	createTask(description: string): void {
+		this.taskList.push(
+			new Task(description, this.taskList.length)
+		);
 	}
 }
