7b4c4f11cc915dd1d62c93fab5d2f79cd7fa646f
Michael Gallegos <mklgallegos@gmail.com>
2016-03-22 11:14:55 -0700

    finish Displaying data in a view

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,17 +5,36 @@ import { Component } from 'angular2/core';
 // It names our HTML tag under the selector property, and lists the HTML we want to display under the template property.
 // The @Component() defining our annotation is called a decorator
 // The annotation part of a component is like a blueprint - it tells Angular that the class declaration you are about to write is for a component, which has a particular structure built into Angular where it expects a selector and a template at least.
+// *ngFor is an Angular directive, it's a special keyword that you can use as an attribute in your HTML. *ngFor creates a for each loop in your template when we say: <h3 *ngFor="#task of tasks">{{ task.description }}</h3> 
+
+//The *ngFor directive will duplicate the HTML element it is bound to once for each element in the specified array - in our case tasks. If there are any HTML elements nested inside the element with the *ngFor attribute, then the entire block is copied.
+
 @Component({
 	selector: 'my-app',
 	template: `
 		<div class="container">
-			<h1>Skeleton Angular2 App!</h1>
+			<h1>To-Do List</h1>
+			<h3 *ngFor="#task of tasks">{{ task.description }}</h3>
 		<div>
-	`
+		`
 })
 
 // class
 // The component's class declaration holds the data and methods needed to make the template HTML functional.
-export class AppComponent {
 
+export class AppComponent { //Controller class definition
+	public tasks: Task[];
+	constructor(){
+		this.tasks = [
+			new Task("Create To-Do List app.", 0),
+			new Task("Learn Kung Fu.", 1),
+			new Task("Rewatch all the Lord of the Rings movies.", 2),
+			new Task("Do the laundry.", 3)
+		];
+	}
+}
+
+export class Task {
+	public done: boolean = false; // done property will have default value of false
+	constructor(public description: string, public id: number){}
 }
