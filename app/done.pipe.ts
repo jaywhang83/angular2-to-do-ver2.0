989179c5ad988a20b7dd57904559b34602df3adb
Michael Gallegos <mklgallegos@gmail.com>
2016-03-22 14:57:21 -0700

    finish Using pipes

---
+++ app/done.pipe.ts
@@ -0,0 +1,25 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+import {Task} from './task.model';
+
+@Pipe({
+	name: "done", // The name is what we will use to refer to the pipe when we implement it in TaskListComponent
+	pure: false //false indicates stateful pipe
+
+})
+
+export class DonePipe implements PipeTransform {
+	transform(input: Task[], args) {
+		var desiredDoneState = args[0];
+		if(desiredDoneState === "done") {
+			return input.filter((task) => {
+				return task.done;
+			});
+		} else if (desiredDoneState === "notDone") {
+			return input.filter((task) => {
+				return !task.done;
+			});
+		} else {
+			return input;
+		}
+	}
+}
--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,6 +3,7 @@ import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
+import { DonePipe } from './done.pipe';
 
 
 
@@ -11,11 +12,17 @@ import { NewTaskComponent } from './new-task.component';
 	selector: 'task-list',
 	inputs: ['taskList'],
 	outputs: ['onTaskSelect'],
+	pipes: [DonePipe],
 	directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent], // these are all children
 	//templateUrl: 'app/task-list.component.html' // If we use templateUrl, we just have to remember that the path is relative to the top level of your project directory because this is where we start our server. So, it's important to include the app/ at the beginning of the templateUrl path, otherwise it won't be located.
 	template:
 	`
-	<task-display *ngFor="#currentTask of taskList"
+	<select (change)="onChange($event.target.value)" class="filter">
+	  <option value="all">Show All</option>
+	  <option value="done">Show Done</option>
+	  <option value="notDone" selected="selected">Show Not Done</option>
+	</select>
+	<task-display *ngFor="#currentTask of taskList | done: filterDone"
 		(click)="taskClicked(currentTask)"
 		[class.selected]="currentTask == selectedTask"
 		[task]="currentTask">
@@ -30,6 +37,8 @@ export class TaskListComponent {
 	public taskList: Task[];
 	public onTaskSelect: EventEmitter<Task>;
 	public selectedTask: Task;
+	public filterDone: string = "notDone"; // First, let's create a property in our component controller class to store the value from this menu. We'll set it to a default value of "notDone", since tasks that are not done yet are the first priority for Cameron to see.
+	
 	constructor() {
 		this.onTaskSelect = new EventEmitter();
 	}
@@ -44,5 +53,10 @@ export class TaskListComponent {
 		this.taskList.push(
 			new Task(description, this.taskList.length)
 		);
+	}
+
+	onChange(filterOption) {
+		this.filterDone = filterOption;
+		console.log(this.filterDone);
 	}
 }
